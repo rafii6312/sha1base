cc79b8695d93ccc315d616cc5e1da561db5b32f8
rafii6312 <rafig.ossami@googlemail.com>
2014-02-22 16:14:54 +0100

    encrypt module added

---
+++ modules/s1b_encrypt.php
@@ -0,0 +1,17 @@
+<?php
+
+class sha1base_filesystem extends sha1base
+{
+	public $encryption = MCRYPT_RIJNDAEL_256;
+	
+	
+	public function __construct()
+	{
+		echo $encryption;
+	}
+	
+	
+}
+
+
+?>
